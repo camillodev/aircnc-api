704c760a778848f71957240b07afd5284e0aaee6
Rafael Camillo <camillodeveloper@gmail.com>
2019-10-02 20:57:13 -0300

    bring thumbnail url

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const routes = require("./routes");
 const mongoose = require("mongoose");
 const cors = require('cors');
+const path = require('path');
 
 mongoose.connect(
   "mongodb+srv://rafaelcamillo:4CarpeDiem!@general-7dtp4.mongodb.net/omnistack?retryWrites=true&w=majority",
@@ -15,6 +16,7 @@ const app = express();
 
 app.use(cors('http://localhost:3000'))
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(5000);
