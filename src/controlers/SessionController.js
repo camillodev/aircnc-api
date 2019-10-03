21358e965a06e9409cf3ad3cc303bf7b5dc9482d
Rafael Camillo <camillodeveloper@gmail.com>
2019-10-03 16:32:35 -0300

    remove consoles

--- src/controlers/SessionController.js
+++ src/controlers/SessionController.js
@@ -15,18 +15,12 @@ module.exports = {
   },
 
   async findUserByEmail(req, res) {
-    var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-    console.log("FullURL " + fullUrl)
-
     const { email } = req.query;
-    console.log(email)
-
 
     let user = await User.findOne({ email });
     if (!user) {
      return res.json({error: "user not founded"});
     }
-    
     return res.json(user);
   }
 
